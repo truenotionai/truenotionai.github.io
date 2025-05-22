43e66459f516f0bdbef3805a82b0c0f8b1eaf3b7
SARVESH TELANG <telang.sarvesh98@gmail.com>
2025-05-22 23:22:56 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // ✅ Correct for username.github.io repo
   server: {
     host: true,
     strictPort: true,
@@ -11,5 +10,6 @@ export default defineConfig({
   },
   define: {
     global: 'window',
-  }
+  },
+  base: '/True-Notion-AI/'
 })
